cb88541e95c30164334499b914de94fbb01b9e94
akhilsai0099 <akhilsai0099@gmail.com>
2025-06-06 14:37:29 +0530

    Added mini comments and mini pairs

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.diagnostic.config({
-  virtual_text = true,  -- enables inline diagnostics
+	virtual_text = true, -- enables inline diagnostics
 })
 
 vim.opt.relativenumber = true
 vim.opt.number = true
-vim.cmd('filetype plugin on')
-vim.o.omnifunc = 'syntaxcomplete#Complete'
+vim.cmd("filetype plugin on")
+vim.o.omnifunc = "syntaxcomplete#Complete"
 
 require("config.lazy")
