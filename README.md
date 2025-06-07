620e71f0abe841c846ebe5a2be14559960f066fe
akhilsai0099 <akhilsai0099@gmail.com>
2025-06-07 12:46:24 +0530

    Added ReadMe.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## Requirements
+
+- Lazygit
+- c compiler(cc, gcc, zig)
+- ripgrep,
+- nerdfont,
+
+If trying to install any language servers, linters, formatters, make sure to have the package managers of those languages installed in the system
+
+- Javascript/typescript => npm
+- python => pip
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  opts = {
-    auto_install = true,    
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = {
+            enable = true,
+        },
     },
-    indent = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
-
