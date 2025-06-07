88a76cc64c69a18e952f9fe0f73474d46111f082
akhilsai-boppudi-drishyaai <akhilsai.boppudi@drishya.ai>
2025-06-07 12:03:38 +0530

    Added lsp for github-actions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,49 +1,55 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      {
+    {
         "mason-org/mason.nvim",
-        opts = {},
-      },
-      "neovim/nvim-lspconfig",
+        config = function()
+            require("mason").setup()
+        end,
     },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "eslint" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      vim.lsp.config("lua_ls", {
-        settings = {
-          ["lua_ls"] = {},
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            {
+                "mason-org/mason.nvim",
+                opts = {},
+            },
+            "neovim/nvim-lspconfig",
         },
-        capabilities = capabilities,
-      })
-      vim.lsp.config("ts_ls", {
-        settings = {
-          ["ts_ls"] = {},
-        },
-        capabilities = capabilities,
-      })
-      vim.lsp.config("eslint-lsp", {
-        settings = {
-          ["eslint-lsp"] = {},
-        },
-        capabilities = capabilities,
-      })
-      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "ts_ls", "eslint" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    ["lua_ls"] = {},
+                },
+                capabilities = capabilities,
+            })
+            vim.lsp.config("ts_ls", {
+                settings = {
+                    ["ts_ls"] = {},
+                },
+                capabilities = capabilities,
+            })
+            vim.lsp.config("eslint", {
+                settings = {
+                    ["eslint"] = {},
+                },
+                capabilities = capabilities,
+            })
+            vim.lsp.config("gh_actions_ls", {
+                settings = {
+                    ["gh_actions_ls"] = {},
+                },
+                capabilities = capabilities,
+            })
+            vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,25 +1,26 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        --require("none-ls.diagnostics.eslint"),
-      },
-    })
-    -- Format on save
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function()
-        vim.lsp.buf.format({ async = false })
-      end,
-    })
-    vim.keymap.set("n", "<leader>gff", vim.lsp.buf.format, {})
-  end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                -- null_ls.builtins.formatting.yamlfmt,
+                -- null_ls.builtins.diagnostics.kube_linter,
+            },
+        })
+        -- Format on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function()
+                vim.lsp.buf.format({ async = false })
+            end,
+        })
+        vim.keymap.set("n", "<leader>gff", vim.lsp.buf.format, {})
+    end,
 }
